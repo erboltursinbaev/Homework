378287a031f17b2e98775be812ab81d3aa87eee4
erboltursinbaev <etursinbaev7@gmail.com>
2026-04-02 00:15:22 +0500

    Refactor todo management functions and UI elements

--- js76/js/main.js
+++ js76/js/main.js
@@ -1,115 +1,96 @@
 "use strict";
 
 const todoKeys = {
-  id: "id",
-  text: "text",
-  is_completed: "is_completed",
+	id: "id",
+	text: "text",
+	is_completed: "is_completed",
 };
 
 const todos = [];
 
-const errTodoNotFound = todoId => `Todo with id ${todoId} not found`;
+const errTodoNotFound = (todoId) => `Todo with id ${todoId} not found`;
 
-const getNewTodoId = todos =>
-  todos.reduce((maxId, todo) => Math.max(maxId, todo[todoKeys.id]), 0) + 1;
+const getNewTodoId = (todos) =>
+	todos.reduce((maxId, todo) => Math.max(maxId, todo[todoKeys.id]), 0) + 1;
 
 const createTodo = (todos, text) => {
-  const newTodo = {
-    [todoKeys.id]: getNewTodoId(todos),
-    [todoKeys.text]: text,
-    [todoKeys.is_completed]: false,
-  };
-  todos.push(newTodo);
-  return newTodo;
+	const newTodo = {
+		[todoKeys.id]: getNewTodoId(todos),
+		[todoKeys.text]: text,
+		[todoKeys.is_completed]: false,
+	};
+	todos.push(newTodo);
+	return newTodo;
 };
 
 const completeTodoById = (todos, todoId) => {
-  const todo = todos.find(todo => todo[todoKeys.id] === todoId);
-
-  if (!todo) {
-    console.error(errTodoNotFound(todoId));
-    return null;
-  }
-  todo[todoKeys.is_completed] = !todo[todoKeys.is_completed];
-  return todo;
+	const todo = todos.find((todo) => todo[todoKeys.id] === todoId);
+	if (!todo) {
+		console.error(errTodoNotFound(todoId));
+		return null;
+	}
+	todo[todoKeys.is_completed] = !todo[todoKeys.is_completed];
+	return todo;
 };
 
 const deleteTodoById = (todos, todoId) => {
-  const todoIndex = todos.findIndex(todo => todo[todoKeys.id] === todoId);
-  if (todoIndex === -1) {
-    console.error(errTodoNotFound(todoId));
-    return todos;
-  }
-  todos.splice(todoIndex, 1);
-  return todos;
+	const todoIndex = todos.findIndex((todo) => todo[todoKeys.id] === todoId);
+	if (todoIndex === -1) {
+		console.error(errTodoNotFound(todoId));
+		return todos;
+	}
+	todos.splice(todoIndex, 1);
+	return todos;
 };
 
-// При помощи метода querySelector получаем элементы .form, .input и .todos
-// Создаем функцию createTodoElement(text), которая будет создавать todo в виде разметки
-// Создаем функцию handleCreateTodo(todos, text), которая будет вызывать createTodo и createTodoElement
-const form = document.querySelector(".form");       
-const input = document.querySelector(".input");      
-const todosContainer = document.querySelector(".todos"); 
+// UI
+const formElement = document.querySelector(".form");
+const inputElement = document.querySelector(".input");
+const todosElement = document.querySelector(".todos");
 
 const createTodoElement = (todo) => {
-  
-  const li = document.createElement("li");
-  li.classList.add("todo");
-  li.dataset.id = todo[todoKeys.id]; 
-
-  const textDiv = document.createElement("div");
-  textDiv.classList.add("todo-text");
-  textDiv.textContent = todo[todoKeys.text];
-
-  const actionsDiv = document.createElement("div");
-  actionsDiv.classList.add("todo-actions");
-
-  const completeBtn = document.createElement("button");
-  completeBtn.classList.add("button-complete", "button");
-  completeBtn.innerHTML = "&#10004;";
-
-  const deleteBtn = document.createElement("button");
-  deleteBtn.classList.add("button-delete", "button");
-  deleteBtn.innerHTML = "&#10006;";
-
-  actionsDiv.appendChild(completeBtn);
-  actionsDiv.appendChild(deleteBtn);
-
-  li.appendChild(textDiv);
-  li.appendChild(actionsDiv);
-
-  return li;
+	const todoElement = document.createElement("li");
+	todoElement.classList.add("todo");
+	todoElement.dataset.id = todo[todoKeys.id];
+	todoElement.innerHTML = `
+	<div class="todo-text">${todo[todoKeys.text]}</div>
+  <div class="todo-actions">
+		<button class="button-complete button">&#10004;</button>
+		<button class="button-delete button">&#10006;</button>
+	</div>
+	`;
+	return todoElement;
 };
 
 const handleCreateTodo = (todos, text) => {
-  
-  const newTodo = createTodo(todos, text);
-
-  const todoEl = createTodoElement(newTodo);
-
-  todosContainer.appendChild(todoEl);
+	const todo = createTodo(todos, text);
+	const todoElement = createTodoElement(todo);
+	todosElement.prepend(todoElement);
 };
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const text = input.value.trim();
-  if (!text) return;
-  handleCreateTodo(todos, text);
-  input.value = "";
+formElement.addEventListener("submit", (event) => {
+	event.preventDefault();
+
+	const text = inputElement.value.trim();
+	if (!text) return;
+
+	handleCreateTodo(todos, text);
+	inputElement.value = "";
 });
 
-todosContainer.addEventListener("click", (event) => {
+todosElement.addEventListener("click", ({ target }) => {
+	const todo = target.closest(".todo");
+	if (!todo) return;
 
-  const todoEl = target.closest(".todo");
-  if (!todoEl) return;
+	const todoId = Number(todo.dataset.id);
 
-  const id = Number(todoEl.dataset.id);
+	if (target.matches(".button-complete")) {
+		completeTodoById(todos, todoId);
+		todo.classList.toggle("completed");
+	}
 
-  if (target.matches("button-complete")) {
-    completeTodoById(todos, id);
-    todoEl.classList.toggle("completed");
-  } else if (target.matches("button-delete")) {
-    deleteTodoById(todos, id);
-    todoEl.remove();
-  }
+	if (target.matches(".button-delete")) {
+		deleteTodoById(todos, todoId);
+		todo.remove();
+	}
 });
